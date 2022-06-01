19cb4b32be0789e927ec7e94a979921868e29ce7
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-06-01 11:43:49 +0900

    qrで同じidの部屋に入るの実装

--- src/QrCodeReader.jsx
+++ src/QrCodeReader.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
+import { useNavigate } from 'react-router-dom';
+
 
 export const QrCodeReader = (props) => {
     const [data, setData] = useState('No result');
+    const navigate = useNavigate();
 
     return (
         <>
             <QrReader
+                constraints={{ facingMode: 'environment' }}
                 onResult={(result, error) => {
                     if (!!result) {
                         setData(result?.text);
+                        navigate("/app?" + String(result).split('?')[1])
                     }
 
                     if (!!error) {
                         console.info(error);
                     }
                 }}
-                style={{ width: '100%' ,height:"100vh"}}
+                style={{ width: '100%'}}
             />
             <p>{data}</p>
         </>
